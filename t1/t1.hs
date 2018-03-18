68b3b7d9db8a0bd9dfda68aab872176718163dd5
Pedro <pmcarvalho.ufsm.br>
2018-03-18 20:23:47 -0300

    ADD ex 2
    ADD ex 2

--- t1/t1.hs
+++ t1/t1.hs
@@ -7,3 +7,9 @@ import Data.Char
 isVowel :: Char -> Bool
 
 isVowel c = elem (toLower(c)) "aeiou"
+-- 2. Escreva uma função addComma, que adicione uma vírgula no final de cada string contida numa lista.
+
+
+addComma :: [String] -> [String]
+
+addComma s = map(\c -> c ++ ",")s
